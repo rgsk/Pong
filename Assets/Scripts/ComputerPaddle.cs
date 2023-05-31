2d4c6b571d18206454a4e5dabadaa30961eb7273
rgsk <guptarahul70322@gmail.com>
2023-05-31 23:23:44 +0530

    computer paddle movement

--- Assets/Scripts/ComputerPaddle.cs
+++ Assets/Scripts/ComputerPaddle.cs
@@ -1,3 +1,26 @@
+using UnityEngine;
 public class ComputerPaddle : Paddle {
+
+    public Rigidbody2D ball;
+
+    private void FixedUpdate() {
+        if (this.ball.velocity.x > 0.0f) {
+            // ball is moving to the right
+            if (this.ball.position.y > this.transform.position.y) {
+                // ball is above
+                // add force to the paddle to move up
+                _rigidbody.AddForce(Vector2.up * this.speed);
+            } else if (this.ball.position.y < this.transform.position.y) {
+                _rigidbody.AddForce(Vector2.down * this.speed);
+            }
+        } else {
+            if (this.transform.position.y > 0.0f) {
+                _rigidbody.AddForce(Vector2.down * this.speed);
+            } else if (this.transform.position.y < 0.0f) {
+                _rigidbody.AddForce(Vector2.up * this.speed);
+            }
+        }
+
+    }
 
 }
