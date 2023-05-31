d3b9e4805bc13f86c86c742562185dd9f8457f0a
rgsk <guptarahul70322@gmail.com>
2023-06-01 00:27:12 +0530

    text updation

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,14 +6,19 @@ public class Ball : MonoBehaviour {
     private Rigidbody2D _rigidbody;
     private void Awake() {
         _rigidbody = GetComponent<Rigidbody2D>();
-        ResetPosition();
+
     }
+
+    private void Start() {
+        ResetPosition();
+        AddStartingForce();
+    }
+
     public void ResetPosition() {
         _rigidbody.position = Vector2.zero;
         _rigidbody.velocity = Vector2.zero;
-        AddStartingForce();
     }
-    private void AddStartingForce() {
+    public void AddStartingForce() {
         var x = Utils.Toss() ? -1.0f : 1.0f;
         var y = Utils.Toss() ? Random.Range(-1.0f, -0.5f) : Random.Range(0.5f, 1.0f);
         var direction = new Vector2(x, y);
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour {
     public Ball ball;
+
+    public TMP_Text playerScoreText;
+    public TMP_Text computerScoreText;
+
     private int _playerScore;
     private int _computerScore;
     public void PlayerScores() {
         _playerScore++;
-        this.ball.ResetPosition();
+        playerScoreText.text = _playerScore.ToString();
+        ball.ResetPosition();
+        ball.AddStartingForce();
     }
     public void ComputerScores() {
         _computerScore++;
-        this.ball.ResetPosition();
+        computerScoreText.text = _computerScore.ToString();
+        ball.ResetPosition();
+        ball.AddStartingForce();
     }
 
 }
