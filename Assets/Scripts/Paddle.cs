b90b4dcf1d290797570592b6de77b88dae13b9d6
rgsk <guptarahul70322@gmail.com>
2023-05-31 22:26:59 +0530

    pong board setup

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    // Start is called before the 
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
